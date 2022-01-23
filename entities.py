0961d43fcfb33974ce61fee6b2ec8dd4bd7ce18c
zarifatai <zarifatai@github.com>
2022-01-23 17:14:16 +0100

    Added self argument to method classes

--- entities.py
+++ entities.py
@@ -1,5 +1,5 @@
 class Match:
-    def __init__(match_id, status, match_start_iso, minute, referee_id, home_team, away_team, stats, venue):
+    def __init__(self, match_id, status, match_start_iso, minute, referee_id, home_team, away_team, stats, venue):
         self.match_id = match_id
         self.status = status
         self.match_start_iso = match_start_iso
@@ -12,7 +12,7 @@ class Match:
 
 
 class Team:
-    def __init__(team_id, name, short_code, logo):
+    def __init__(self, team_id, name, short_code, logo):
         self.team_id = team_id
         self.name = name
         self.short_code = short_code
@@ -20,7 +20,7 @@ class Team:
 
 
 class MatchStats:
-    def __init__(home_score, away_score, ht_score, ft_score, et_store, ps_score):
+    def __init__(self, home_score, away_score, ht_score, ft_score, et_store, ps_score):
         self.home_score = home_score
         self.away_score = away_score
         self.ht_score = ht_score
@@ -29,7 +29,7 @@ class MatchStats:
         self.ps_score = ps_score
 
 class Venue:
-    def __init__(venue_id, name, capacity, city, country_id):
+    def __init__(self, venue_id, name, capacity, city, country_id):
         self.venue_id = venue_id
         self.name = name
         self.capacity = capacity
